58a13343d8774fd908ea3782e061be26f0e064d2
jroze88 <jano.roze@gmail.com>
2022-04-23 19:23:21 -0400

    cleanup (pt2)

--- client/src/containers/experienceAccordion/ExperienceAccordion.js
+++ client/src/containers/experienceAccordion/ExperienceAccordion.js
@@ -5,11 +5,12 @@ import { Accordion, Panel } from "baseui/accordion";
 import { DarkTheme, LightTheme, ThemeProvider } from "baseui";
 import ProjectCard from "../../components/ProjectCard/ProjectCard.js";
 import { projects } from "../../portfolio.js";
-
+import onTheTableGaming from './wordpressxl.png'
+import { Fade } from "react-reveal";
+import { style } from "glamor";
 
 function ExperienceAccordion(props) {
   const theme = props.theme;
-  const [renderIT, setrenderIT] = useState(false)
 
   return (
     <div className="experience-accord" >
@@ -59,12 +60,12 @@ function ExperienceAccordion(props) {
                 className="accord-panel3"
                 title={"Case Studies"}
                 key={"title323"}
-                onChange={({ expanded }) => setrenderIT(!renderIT)}
+                // onChange={({ expanded }) => setrenderIT(!renderIT)}
               >
 
-             {renderIT ?  <DangerousIframe/> : null}
-             
-                
+             {/* {renderIT ?  <DangerousIframe/> : null} */}
+
+             <WordPressProjectCard  theme={theme} />
                 </Panel>
                 <Panel
                 className="accord-panel4"
@@ -79,13 +80,49 @@ function ExperienceAccordion(props) {
   );
 }
 
-function DangerousIframe(props) {
-  
+function WordPressProjectCard({  theme }) {
+  function openRepoinNewTab(url) {
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+
+  const styles = style({
+    boxShadow: "rgba(0, 0, 0, 0.2) 0px 10px 30px -15px",
+    padding: "2rem",
+    cursor: "pointer",
+    borderRadius: "5px",
+    height: "100%",
+    transition: "all 0.2s ease-in-out",
+    ":hover": {
+      boxShadow: `${theme.imageDark} 0 2px 15px`,
+    },
+  });
 
   return (
-    <div className='dangerous-iframe' dangerouslySetInnerHTML={{ __html: "<iframe style={width: 600px; height: 600px; background-color: grey;} src='https://visual-argo-workflows.vercel.app/' />"}} />
-  )
-
+    <div>
+      <Fade bottom duration={2000} distance="40px">
+        <div
+          {...styles}
+          key={'101'}
+          onClick={() => openRepoinNewTab('https://onthetablegaming.com')}
+          style={{ backgroundColor: theme.body }}
+        >
+          <div className="repo-name-div">
+            <p className="repo-name" style={{ color: theme.secondaryText }}>
+              Game of Thrones Tabletop Community Content Site
+            </p>
+            
+          </div>
+          <p className="repo-description" style={{ color: theme.text }}>
+            Apache Wordpress deploy alongside Node.js with Automated Content Creation from across the Web
+          </p>
+          <img src={onTheTableGaming} className='wordpress-thumb' />
+          <div className="repo-details">
+          </div>
+        </div>
+      </Fade>
+    </div>
+  );
 }
 
 
